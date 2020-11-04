8dacc29159411b15c9d4b1365167f7bc0fd20518
Brandon O <b.ondayko@gmail.com>
2020-11-04 16:24:20 -0600

    api now displays imperial

--- assets/js/homepage.js
+++ assets/js/homepage.js
@@ -1,6 +1,23 @@
 var apiKey = "76244bc3926e8cc4187446fcb65160e1";
+//storage section
+var storedLocations = $("#stored-locations");
+//active section
 var activeCityName = "";
+var temp = $("#temp");
+var humi = $("#humi");
+var wind = $("#wind");
+var uv = $("#uv");
+var activeTitle = $("#active-city");
 
+
+//display the content
+var display = function(data) {
+    //get the name of the city
+    var name = data.city.name;
+    console.log(name);
+    //get the date
+
+}
 
 
 //get the value that was inputed from the search
@@ -18,12 +35,12 @@ var searchName = function() {
 
 //get the 5 day weather forecast
 var fivedayForecast = function(cityName) {
-    var apiUrl =("http://api.openweathermap.org/data/2.5/forecast?q="+cityName+"&appid="+apiKey);
+    var apiUrl =("http://api.openweathermap.org/data/2.5/forecast?q="+cityName+"&units=imperial&appid="+apiKey);
     
     fetch(apiUrl).then(function(response) {
         if(response.ok) {
             response.json().then(function(data) {
-                console.log(data);
+                display(data);
             });
         } else {
             alert("Error: " + response.statusText);
