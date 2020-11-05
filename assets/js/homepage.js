554c6c85f7f880afa108c9af585ead8a0bbf3d2e
Brandon O <b.ondayko@gmail.com>
2020-11-04 18:40:43 -0600

    City and date display on active

--- assets/js/homepage.js
+++ assets/js/homepage.js
@@ -1,4 +1,5 @@
 var apiKey = "76244bc3926e8cc4187446fcb65160e1";
+var apiKey2 = "514eacd0018f9117c6b60ae205c7e3af";
 //storage section
 var storedLocations = $("#stored-locations");
 //active section
@@ -10,16 +11,37 @@ var uv = $("#uv");
 var activeTitle = $("#active-city");
 
 
+//get the date
+var dateFinder = function() {
+    var date = moment().local().format("MM/DD/YY");
+    return date;
+    //console.log(date);
+}
+
 //display the content
 var display = function(data) {
+    //get the date
+    var date = dateFinder();
+
+    console.log(date)
     //get the name of the city
     var name = data.city.name;
     console.log(name);
-    //get the date
-    var date = moment().local().format("MM/DD/YY");
 
-    
-    console.log(date);
+    //get the temp
+    var temper = data.list[0].main.temp + "F";
+    console.log(temper);
+
+    //get the humidity
+    var humidity = data.list[0].main.humidity + "%";
+    console.log(humidity);
+
+    //get the wind
+    var windy = data.list[0].wind.speed + "mph";
+    console.log(windy);
+
+    activeTitle.text(name + " " + date);
+
 }
 
 
@@ -38,7 +60,7 @@ var searchName = function() {
 
 //get the 5 day weather forecast
 var fivedayForecast = function(cityName) {
-    var apiUrl =("http://api.openweathermap.org/data/2.5/forecast?q="+cityName+"&units=imperial&appid="+apiKey);
+   var apiUrl =("http://api.openweathermap.org/data/2.5/forecast?q="+cityName+"&units=imperial&cnt=5&appid="+apiKey);
     
     fetch(apiUrl).then(function(response) {
         if(response.ok) {
